a0f91c5fba8bfc0c5cf0aa38ca6b0509fd666846
Yang <yc2984@gmail.com>
2018-02-01 11:07:02 +0100

    Created a live feed txt file

--- modbus-tk-test.py
+++ modbus-tk-test.py
@@ -13,8 +13,10 @@ import serial
 
 PORT = 'COM10'
 
-
 # PORT = '/dev/ptyp5'
+logpath = r'C:\Users\Yang\Documents\RBES work\Projects&study\Sensors\serial_code'
+filename = 'unique_output.txt'
+filename_long = 'all_output.csv'
 
 def write_file(data, logpath, filename):
     with open(os.path.join(logpath, filename), 'a', encoding="utf-8") as f:  # open log file
@@ -22,8 +24,12 @@ def write_file(data, logpath, filename):
         f.close()
 
 
-logpath = r'C:\Users\Yang\Documents\RBES work\Projects&study\Sensors\serial_code'
-filename = 'test_output.txt'
+glmpath = logpath
+glmtxt = 'glminput.txt'
+
+
+def write_glm_feed(data, glmpath, glmtxt):  # glm is a hydrostatic software
+    data.to_csv(os.path.join(glmpath, glmtxt), index=False,header=None, mode='w')
 
 
 def main():
@@ -48,39 +54,44 @@ def main():
         date_time_series = pd.Series()
         while True:
 
-            #cmd = sys.stdin.readline()
-            #print(cmd)
-            #args = cmd.split(' ')
+            cmd = sys.stdin.readline()
+            print(cmd)
+            args = cmd.split(' ')
 
-            #Read current date time
+            # Read current date time
             time.sleep(3)  # wait for 2 seconds
             current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            #print("current time:" + str(current_date_time))
-            date_time_series = pd.concat([date_time_series,pd.Series(current_date_time)]) #not needed
+            # print("current time:" + str(current_date_time))
+            date_time_series = pd.concat([date_time_series, pd.Series(current_date_time)])  # not needed
 
-            #Read current value
+            # Read current value
             value = slave_1.get_values('block1', 100, 127)
             list_value = list(value)
-            print('current value' + str(value)) #tuple
+            print('current value' + str(value))  # tuple
 
-            list_value.insert(0,current_date_time)  #inserted the datetime, INPLACE. So list_value actully includes the datetime.
-            print("time_and_value:"+str(list_value))
+            list_value.insert(0,
+                              current_date_time)  # inserted the datetime, INPLACE. So list_value actully includes the datetime.
+            print("time_and_value:" + str(list_value))
             print("length of the time and value:" + str(len(list_value)))
 
-            #Log the current value into DataFrame
+            # Log the current value into DataFrame
             values = pd.concat([values, pd.DataFrame(list_value).T], axis=0)
             values.index = date_time_series
-            print("This is the logging dataframe") #logging all the readings.
+            print("This is the logging dataframe")  # logging all the readings.
             print(values.head())
+
+            # export the current value into glm feed txt file
+            # sensor's register no. is the same as the sensor ID in glm
+            df_current_value = pd.DataFrame({'Sensor_ID': range(100, 227), 'Sensor_reading': value})  # try with tuple
+            write_glm_feed(df_current_value,glmpath,glmtxt)
 
             # only write the changed values to txtfile
             # Check if it's the first reading of this record.
-            #if values.empty:
+            # if values.empty:
             if len(values) == 1:
-
                 write_file(list_value, logpath, filename)
                 # check the dimension and the last row of values DataFrame
-                print("The dimension of values DataFrame: %s" %(values.shape,))
+                print("The dimension of values DataFrame: %s" % (values.shape,))
                 print("The last row of values DataFrame" + str(values.iloc[-1, :]))
                 continue
 
@@ -88,18 +99,19 @@ def main():
             df_temp = pd.DataFrame({"Last_row_values": values.iloc[-2, :], "New_reading": pd.Series(list_value)})
             print("This is the comparison DataFrame")
             print(df_temp.head())
-            if (df_temp.loc[1:,'Last_row_values'] == df_temp.loc[1:,'New_reading']).all(): #Don't compare the time.
+            if (df_temp.loc[1:, 'Last_row_values'] == df_temp.loc[1:, 'New_reading']).all():  # Don't compare the time.
                 print("The value has not changed since last time, not recorded")
                 continue
 
-            #values = pd.concat([values, pd.DataFrame(list(value)).T], axis=0)
+            # values = pd.concat([values, pd.DataFrame(list(value)).T], axis=0)
             write_file(list_value, logpath, filename)
             print("The new value is recorded")
 
-            #if cmd.find('quit') == 0:
-            #    sys.stdout.write('bye-bye\r\n')
-            #break
-
+            if cmd.find('quit') == 0:
+                sys.stdout.write('bye-bye\r\n')
+            break
+        #write all the readings of this log to a txt file
+        values.to_csv(os.path.join(glmpath, filename_long),index=None)
     finally:
         server.stop()
 

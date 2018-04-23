b3204c302b3d79f3c0738ac0c2cfdb1928029d97
Yang <yc2984@gmail.com>
2018-04-23 17:06:11 +0200

    Always keep the last 2 min live

--- log_volume.py
+++ log_volume.py
@@ -32,20 +32,19 @@ def read_volume(df_vols, vol_file, sample_period, sample_rate):
     """
     df_current_vol = pd.read_csv(vol_file, header=None)
     current_vol_list = df_current_vol.iloc[:, 1].tolist()
-    num_rows = sample_period / (60 / sample_rate)  # eg. sample_rate 12, means read every 5 seconds
+    num_rows = sample_period/(60/sample_rate) # If sample_rate = 60, sample period 60 seconds, num_rows is 60.
+    print("%d values will be used for average" % num_rows)
+    print("Before adding new record, the df_vols has %d records" % len(df_vols))
 
     if len(df_vols) < num_rows*2-1: # skip the first two average period
-        print("%d values will be used for average" % num_rows)
-        print("Before adding new record, the df_vols has %d records" % len(df_vols))
         df_vols = pd.concat([df_vols, pd.DataFrame(current_vol_list).T], axis=0)
         print("After adding new record, the df_vols has %d records" % len(df_vols))
         return '', '', df_vols, num_rows, len(df_vols)  # The length after append current reading.
     else:
-        print("Before adding new record, the df_vols has %d records" % len(df_vols))
         df_vols = pd.concat([df_vols.tail(int(2*num_rows-1)), pd.DataFrame(current_vol_list).T], axis=0)
         print("After adding new record, the df_vols has %d records" % len(df_vols))
-        average_vol1 = df_vols.iloc[:int(len(df_vols)/2), :].mean(numeric_only=True, axis=0).tolist()
-        average_vol2 = df_vols.iloc[int(len(df_vols)/2):, :].mean(numeric_only=True, axis=0).tolist()
+        average_vol1 = df_vols.iloc[:int(num_rows), :].mean(numeric_only=True, axis=0).tolist()
+        average_vol2 = df_vols.iloc[int(num_rows):, :].mean(numeric_only=True, axis=0).tolist()
         assert isinstance(average_vol1, list)
         assert isinstance(average_vol2, list)
         return average_vol1, average_vol2, df_vols, num_rows, len(df_vols)
@@ -54,13 +53,15 @@ def read_volume(df_vols, vol_file, sample_period, sample_rate):
 def volume_change(vol1, vol2, threshold):
     """ This function compares two list, return True if
     at least one of the tank volume changed more than 50 tons """
+    print("Vol1: ", vol1[:5])
+    print("Vol2: ", vol2[:5])
+    print("The change of the first five values: ", [abs(a - b) for a, b in zip(vol1, vol2)][:5])
+    print("The max change is :", max([abs(a-b) for a, b in zip(vol1, vol2)]))
     if max([abs(a-b) for a, b in zip(vol1, vol2)]) >= threshold:
         print("at least one volume has changed more than %d" % threshold)
-        print("The change of the first five values: ",[abs(a-b) for a, b in zip(vol1, vol2)][:5])
         return True
     else:
         print("no volume has changed more than %d" % threshold)
-        print([abs(a - b) for a, b in zip(vol1, vol2)][:5])
         return False
 
 
@@ -83,7 +84,7 @@ def write_file(data, logpath, filename, header):
         df.to_csv(f, index=None, header=if_header)
 
 
-def main(logpath, sample_rate):
+def main(logpath, sample_rate, sample_period=60):
     """This function logs the volume to a csv file when:
     compared with last minute average
     at least one of the tank volume changed more than 50 tons
@@ -95,10 +96,10 @@ def main(logpath, sample_rate):
         with open(r"C:\Users\Yang\Documents\RBES work\Projects&study\Sensors\serial_code\mode.txt",'r') as f:
             log_mode = f.read()
 
-        print("you're here")
-        sample_period = 60
-        if log_mode == "LOAD" :
-            sample_period = 10
+        print("\n")
+        print("Start")
+        #if log_mode == "LOAD" :
+            # sample_period = 10
 
         time.sleep(60 / sample_rate)
         print("It's %s mode" %log_mode)
@@ -112,18 +113,20 @@ def main(logpath, sample_rate):
             continue
 
         current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        current_time_vol_list = [current_date_time] + average_vol2
+        current_time_vol_list = [current_date_time] + [log_mode] + average_vol2
         print("length of current_data_list", len(average_vol2))
         print("length of the data to log:", len(current_time_vol_list))
-        log_header = ["Time"] + tanknames
+        log_header = ["Time"] + ["log_mode"] + tanknames
         print("length of the header",len(log_header))
 
         if volume_change(average_vol1, average_vol2, 50) or log_mode == "LOAD":
             write_file(current_time_vol_list, logpath, vol_daily_file, log_header)
             print("new value has been logged")
+        else:
+            print("not logged")
 
 
 if __name__ == "__main__":
-    main(logpath, 30)  # logpath, sample_rate (no.of readings per minute)
+    main(logpath, 60)  # logpath, sample_rate (no.of readings per minute), by default read measurement every second
 
 
